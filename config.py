cb78f634c1b0ee1ce216dcce47b6f0126171567b
jujaehong <kr208h@gmail.com>
2023-06-22 09:56:33 +0900

    up

---
+++ config.py
@@ -0,0 +1,18 @@
+
+
+class Config :
+
+    # DB   관련정보
+
+    HOST = 'yhdb1004.co4tgwoguyyt.ap-northeast-2.rds.amazonaws.com'
+    DATABASE = 'memo_db2'
+    DB_USER = 'memo_db2_user'
+    DB_PASSWORD = '1234'
+
+    SALT = '0619@HELLO~'
+
+
+    # JWT 변수 셋팅 ( ID 암호화 )
+    JWT_SECRET_KEY = 'hello~!by@' # 아무글자 또는 기호를 넣어준다.
+    JWT_ACCESS_TOKEN_EXPIRES = False   # 자동로그인 해제 또는 유지하도록 세팅
+    PROPAGATE_EXCEPTIONS = True  
